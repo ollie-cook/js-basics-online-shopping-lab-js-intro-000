7b856e25362ab1a19d4754e30179754564a0bccf
Oliver Cook <olliewatford@gmail.com>
2020-06-24 14:54:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- cart.push({itemName:item,itemPrice:100*Math.floor(Math.random()));
+ cart.push({itemName:item,itemPrice:100*Math.floor(Math.random())});
 }
 
 function viewCart() {
