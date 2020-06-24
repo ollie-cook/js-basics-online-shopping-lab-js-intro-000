43edee49827090b55a886c82cd65e735247c2970
Oliver Cook <olliewatford@gmail.com>
2020-06-24 15:35:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,9 @@ function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber){
    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-   cart=[];
+   for (let i=0;i<cart.length;i++){
+     delete cart[i]
+   }
  }else{
    return 'Sorry, we don\'t have a credit card on file for you.'
  }
