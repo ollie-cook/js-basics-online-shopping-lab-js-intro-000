ed22e9acafee6882efd5cbdde642e289a439abfd
Oliver Cook <olliewatford@gmail.com>
2020-06-24 15:38:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,8 +63,9 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber){
-   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-   cart.length=0
+   var finalTotal = total()
+   cart=[]
+   return `Your total cost is $${finalTotal}, which will be charged to the card ${cardNumber}.`
  }else{
    return 'Sorry, we don\'t have a credit card on file for you.'
  }
